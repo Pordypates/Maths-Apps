afa181b35b43b69c0f37dd6afe273374274bc82b
Jordan Yates <42977741+Pordypates@users.noreply.github.com>
2019-06-01 10:18:54 +0100

    Create EvenOdd.java
    Importing EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,9 @@
+public class EvenOdd {
+    public static void EvenOrOdd(int number){
+        if (number % 2 == 0) {
+            System.out.println("Number " + number + " is even!");
+        } else{
+            System.out.println("Number " + number + " is odd!");
+        }
+    }
+}

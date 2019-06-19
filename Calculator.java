5d65ea3a64768deea0d19a51a9ef689a3cacd180
Jordan Yates <42977741+Pordypates@users.noreply.github.com>
2019-06-19 21:55:51 +0100

    Update Calculator.java
    Separated calculation types by methods and implemented calculation select method

--- Calculator.java
+++ Calculator.java
@@ -1,46 +1,94 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Calculator {
 
+    static int num_1;
+    static int num_2;
     static double answer;
+    static String calculation;
 
     public static String[] calculations = {"Addition", "Subtraction", "Multiplication", "Division", "Modulus",
             "Exponentiation", "Square Root" };      // best practice for long array code?
 
-    public static void Addition(double num_1, double num_2){
+
+    public static void CalculationSelect(){     // is there any point having separate methods for each calculation?
+
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Please select a calculation from the following and then the first and second integer.");
+
+        System.out.println(Arrays.toString(calculations));
+
+        calculation = myObj.nextLine();
+        num_1 = myObj.nextInt();
+        num_2 = myObj.nextInt();
+
+        switch (calculation) {
+            case "Addition":
+                Addition();
+                break;
+            case "Subtraction":
+                Subtraction();
+                break;
+            case "Multiplication":
+                Multiplication();
+                break;
+            case "Division":
+                Division();
+                break;
+            case "Modulus":
+                Modulus();
+                break;
+            case "Exponentiation":
+                Exponentiation();
+                break;
+            default:
+                System.out.println("This is not a valid calculation. Please select another.");
+                CalculationSelect();
+        }
+    }
+
+    public static void Addition(){
         answer = num_1 + num_2;
-        System.out.println(num_1 " + " num_2 " = " + answer);
+        System.out.println(num_1 + " + " + num_2 + " = " + answer);
+        CalculationSelect();
     }
 
-    public static void Subtraction(double num_1, double num_2){
+    public static void Subtraction(){
         answer = num_1 - num_2;
-        System.out.println(num_1 " - " num_2 " = " + answer);
+        System.out.println(num_1 + " - " + num_2 + " = " + answer);
+        CalculationSelect();
     }
 
-    public static void Multiplication(double num_1, double num_2){
+    public static void Multiplication(){
         answer = num_1 * num_2;
-        System.out.println(num_1 " x " num_2 " = " + answer);
+        System.out.println(num_1 + " x " + num_2 + " = " + answer);
+        CalculationSelect();
     }
 
-    public static void Division(double num_1, double num_2){
+    public static void Division(){
         answer = num_1 / num_2;
-        System.out.println(num_1 " ÷ " num_2 " = " + answer);
+        System.out.println(num_1 + " ÷ " + num_2 + " = " + answer);
+        CalculationSelect();
     }
 
-    public static void Modulus(double num_1, double num_2){
+    public static void Modulus(){
         answer = num_1 % num_2;
-        System.out.println("The remainder of " + num_1 + " ÷ " + num_2 +  " is: " + answer);
+        System.out.println("The remainder of " + num_1 + " ÷ " + num_2 + " is: " + answer);
+        CalculationSelect();
     }
 
-    public static void Exponentiation(double num_1, double num_2){
+    public static void Exponentiation(){
 
-        double answer = Math.pow(num_1, num_2);
-
+        answer = Math.pow(num_1, num_2);
         System.out.println(num_1 + " to the power of " + num_2 + " is: " + answer);
+        CalculationSelect();
     }
 
-    public static void SquareRoot(double num){
-
-        double answer = Math.sqrt(num);
-
-        System.out.println("The square root of " + num + " is: " + answer);
-    }
+//    public static void SquareRoot(double num){
+//        answer = Math.sqrt(num);
+//        System.out.println("The square root of " + num + " is: " + answer);
+//        CalculationSelect();
+//    }
 }
